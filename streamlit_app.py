85300a78e1f260c133e0ca1899ccd447312c9d26
Priya Gaddam <109093721+PRIYABI@users.noreply.github.com>
2023-02-25 19:55:30 -0600

    Update streamlit_app.py
    Added items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
+
+streamlit.items('Omega 3 & Blueberry Oatmeal')
+streamlit.items('Kale, spinach & Rocket Smoothie')
+streamlit.items('Hard-Boiled Free-Range Egg')
