1fb7e5f5b7ad2e34c88f64b85bf46b29a8fab99a
Priya Gaddam <109093721+PRIYABI@users.noreply.github.com>
2023-02-25 19:56:20 -0600

    Update streamlit_app.py
    corrected the function calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.items('Omega 3 & Blueberry Oatmeal')
-streamlit.items('Kale, spinach & Rocket Smoothie')
-streamlit.items('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
